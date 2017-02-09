8e468adb201f8739edd8f2968810fa2fefa107a0
Sebastian Gomez <seagomezar@gmaill.com>
2017-02-08 20:38:55 -0500

    Referencia a la base de datos y dialog nombre creamos el primer Dialogo para introducir el nombre del asado, también obtenemos la referencia a la base de datos con Javascript normal y la almacenamos en una variable

--- public/principal.js
+++ public/principal.js
@@ -1,9 +1,29 @@
-(function(){
+(function () {
 
     var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
 
-    asaoApp.controller('PrincipalCtrl', ['$scope', function($scope){
-        console.log('Estamos Adentro!');
-    }]);
+    asaoApp.controller('PrincipalCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$mdDialog',
+        function ($scope, $firebaseArray, $firebaseObject, $mdDialog) {
+            var vm = this;
+            var ref = firebase.database().ref().child("asados"); // referencia a los asados
+            vm.nuevoAsado = function () {
+
+                    var confirm = $mdDialog.prompt()
+                        .title('¿Cuál es el nombre del asado?')
+                        .placeholder('Asao Peruano!')
+                        .targetEvent()
+                        .ok('Seguir')
+                        .cancel('Me arrepentí');
+
+                    $mdDialog.show(confirm).then(function (result) {
+                        console.log('el asado se va a llamar', result);
+                    }, function () {
+                        console.log('se arrepintió');
+                    });
+
+                vm.asadosArray = $firebaseArray(ref);
+
+            };
+        }]);
 
 })();
