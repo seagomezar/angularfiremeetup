459c35a7f7051741df1639596ea35db43afb3e8b
Sebastian Gomez <seagomezar@gmaill.com>
2017-02-08 20:15:40 -0500

    Angular material y dos botones en el medio

--- public/principal.js
+++ public/principal.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var asaoApp = angular.module('asaoApp', ['firebase']);
+    var asaoApp = angular.module('asaoApp', ['firebase', 'ngMaterial']);
 
     asaoApp.controller('PrincipalCtrl', ['$scope', function($scope){
         console.log('Estamos Adentro!');
